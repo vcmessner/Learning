be9c90f2b8f87bc85e5beb9bd7f44f1fca676ae3
Victor Cracel <vcmessner@hotmail.com.br>
2023-01-30 15:56:01 -0300

    removed some folders to maintaince

--- Flask/Login/models.py
+++
@@ -1,8 +0,0 @@
-from . import LoginDB
-from flask_login import UserMixin
-
-class User(UserMixin,LoginDB.Model):
-    id = LoginDB.Column(LoginDB.Integer, primary_key=True)
-    email = LoginDB.Column(LoginDB.String(20), unique=True)
-    password = LoginDB.Column(LoginDB.String(20))
-    name = LoginDB.Column(LoginDB.String(20))
