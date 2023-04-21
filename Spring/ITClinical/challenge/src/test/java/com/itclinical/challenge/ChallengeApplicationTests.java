293e4f18d97764656938e23d45b17a5e0eea2bf6
Victor Cracel <vcmessner@hotmail.com.br>
2023-04-21 16:24:07 -0300

    added spring folder

---
+++ Spring/ITClinical/challenge/src/test/java/com/itclinical/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itclinical.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
