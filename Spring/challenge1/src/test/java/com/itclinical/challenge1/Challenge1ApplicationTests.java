28d4a92f3d2c2d2f95c8bae36130ea01931a8972
Victor Cracel <vcmessner@hotmail.com.br>
2023-04-24 00:10:09 -0300

    Assert Number as integer + String test Start

--- Spring/challenge1/src/test/java/com/itclinical/challenge1/Challenge1ApplicationTests.java
+++ Spring/challenge1/src/test/java/com/itclinical/challenge1/Challenge1ApplicationTests.java
@@ -3,6 +3,8 @@ package com.itclinical.challenge1;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,36 +21,36 @@ class Challenge1ApplicationTests {
 
     @RepeatedTest(1000)
     void test_Assert_Number_as_Integer(){
-        /*               TODO 
-            Generate a random number A
-            Convert A to String
-            AssertTrue
-            Generate a random number string
-            Assert true 
-            */
-            int a=0;
-            String c="1";
-           // int a = generateRandomnumber();
-            String b = Integer.toString(a);
-            assertTrue(b.matches("^-?[0-9]+"));
-		    assertFalse(b.matches("[A-Z]+"));
-		    assertFalse(b.matches("[a-z]+"));
-          //  String c = RandomNumericStringGen();
-            assertTrue(c.matches("^-?[0-9]+"));
-		    assertFalse(c.matches("[A-Z]+"));
-		    assertFalse(c.matches("[a-z]+"));
-
-
-    }
+		int probabilidade =50;
+		int tamanho =10;
+		String resp ="";
+		Random ran = new Random();
+        int mynumber = ran.nextInt(1000);
+		if(ran.nextInt(100)+1>probabilidade){
+			resp+="-";
+		}
+		for(int i=0;i<tamanho;++i){
+        	resp+=Integer.toString(ran.nextInt(10)); //[0,9[		
+		}
+		assertTrue(resp.matches("^-?[0-9]+"));
+		assertFalse(resp.matches("[A-Z]+"));
+		assertFalse(resp.matches("[a-z]+"));
+        assertTrue(Integer.toString(mynumber).matches("^-?[0-9]+"));
+        assertFalse(Integer.toString(mynumber).matches("[A-Z]+"));
+		assertFalse(Integer.toString(mynumber).matches("[a-z]+"));
+	}
 
     @Test 
     void test_Assert_N_as_Integer(){
         /*               TODO 
             Get Input Parameter N
-            Check if N is a Integer
-            AssertTrue
-
         */
+
+		String N="1";
+        assertTrue(N.matches("^-?[0-9]+"));
+		assertFalse(N.matches("[A-Z]+"));
+		assertFalse(N.matches("[a-z]+"));
+
 
     }
 
@@ -59,6 +61,12 @@ class Challenge1ApplicationTests {
             AssertTrue
 
         */
+
+		//String a = randomLowerCaseAlpha();
+        String LowerCaseAlpha = "asdfg";
+        assertTrue(LowerCaseAlpha.matches("[a-z]+"));
+        assertFalse(LowerCaseAlpha.matches("[A-Z]+"));
+        assertFalse(LowerCaseAlpha.matches("[A-Z]+"));
     }
 
     void test_Assert_String_as_LowerCaseAlphaNum(){
@@ -66,8 +74,9 @@ class Challenge1ApplicationTests {
             Generate a random lowercase Alphanum String
             Granted WrongInput for later
             AssertTrue
-
         */
+		String LowerCaseAlphaNum = "123454";
+        assertTrue(LowerCaseAlphaNum.matches("[a-z0-9]+"));
     }
 
 
@@ -76,8 +85,9 @@ class Challenge1ApplicationTests {
             Generate a random UpperCase String
             Granted correct Input for later
             AssertTrue
-
         */
+		String UpperCaseAlpha = "ABCD";
+        assertFalse(UpperCaseAlpha.matches("[A-Z]+"));
     }
 
     @RepeatedTest(1000)
